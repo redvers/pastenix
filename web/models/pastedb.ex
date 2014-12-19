5cefa3ff64ad4efeda924e8602a2a0a62aa07954
Redvers Davies <red@pwn.me>
2014-12-18 22:10:05 -0500

    Added docs to Pastenix.Paste

--- web/models/pastedb.ex
+++ web/models/pastedb.ex
@@ -2,10 +2,23 @@ use Amnesia
 
 defdatabase PasteDB do
   deftable Paste, [ :id, :title, :public, :ircchannels, :date, :expires, :previous_version, :content], type: :ordered_set do end
+  @type t :: %Paste{ id: String.t, title: String.t, public: boolean, ircchannels: [atom], date: integer, expires: integer, previous_version: String.t, content: String.t}
 end
 
 defmodule Pastenix.Paste do
-## Create Paste
+  @moduledoc """
+  Pastenix.Paste is the module that provides the Interface to the back-end
+  Mnesia database.  The abstraction is provided so:
+    0. Paste storage can be seamlessly moved in the future 
+    1. Updates to Agents which do things like store last X public posts
+        can be updated directly instead of doing continuous arbitrary
+        Mnesia queries.
+  """
+
+  @doc """
+  Takes a record of the type %PasteDB.Paste and returns the assigned UUID which
+  is used to reference it in the future.
+  """
   def create(record) do
     uuid = UUID.uuid1
     Amnesia.transaction do
@@ -15,16 +28,23 @@ defmodule Pastenix.Paste do
     uuid
   end
   
-## View Paste
 
+  @doc """
+  Takes a string which contains the id: which is used to retrieve the specified Paste.
+  Returns the Paste in %PasteDB.Paste record format.
+  """
   def fetch(id) do
     Amnesia.transaction do
-      record = PasteDB.Paste.read(id)
+      PasteDB.Paste.read(id)
     end
   end
     
 ## Edit Paste
 
+  @doc """
+  Takes a Paste in record format and creates a new, linked Paste.  Returns the id: of
+  the new record.
+  """
   def edit(record) do
     originalid = record.id
     uuid = UUID.uuid1
